293159cf947ed98947b97a07d46045804045f6f8
d-georgiev-91 <d.georgiev.91@gmail.com>
2019-08-19 17:39:53 +0300

    Added string encoder to encode/decode string in url percent style

---
+++ Daikin-Controller/Serializers/Encoder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace DaikinController.Serializers
+{
+    public static class Encoder
+    {
+        public static string Encode(string @string)
+        {
+            var buffer = new StringBuilder();
+
+            foreach (var character in @string)
+            {
+                buffer.Append($"%{(int) character:X}");
+            }
+
+            return buffer.ToString();
+        }
+
+        public static string Decode(string @string)
+        {
+            return Uri.UnescapeDataString(@string);
+        }
+    }
+}
